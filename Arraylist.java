66c0f721a23a673f6fba746b6482bc680eb3ed63
Indhuja-developer9363 <gopalindhuja@gmail.com>
2026-06-25 09:55:34 +0530

    Arraylist

---
+++ Arraylist.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class Arraylist{
+    public static void main(String[]args){
+
+        ArrayList<String>fruits = new ArrayList<>();
+        fruits.add("Apple");
+        fruits.add("Banana");
+        fruits.add("Grapes");
+        fruits.add("Mango");
+        System.out.println(fruits);
+        System.out.println(fruits.size());
+    }
+}
