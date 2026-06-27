cc7052039dd20d1af6dd6831e20c3d5c8a01db4f
Indhuja-developer9363 <gopalindhuja@gmail.com>
2026-06-27 11:32:13 +0530

    Constructors

--- Student.java
+++ Student.java
@@ -1,17 +1,39 @@
- class Student{
+//  class Student{
+//         String name;
+//         int age;
+    
+//     Student(){
+//         name = "Indhu";
+//         age = 19;
+// }
+//     void display(){
+//         System.out.println(name);
+//         System.out.println(age);
+//     }
+//     public static void main(String[]args){
+//         Student s1 = new Student();
+//         s1.display();
+//     }
+// }
+
+class Student{
         String name;
         int age;
     
-    Student(){
-        name = "Indhu";
-        age = 19;
+    Student(String n,int a){
+        name = n;
+        age = a;
+}
+Student(Student s){
+    name = s.name;
+    age = s.age;
 }
     void display(){
-        System.out.println(name);
-        System.out.println(age);
+        System.out.println(name + " " + age);
     }
     public static void main(String[]args){
-        Student s1 = new Student();
-        s1.display();
+        Student s1 = new Student("Indhu",19);
+        Student s2 = new Student(s1);
+        s2.display();
     }
 }
