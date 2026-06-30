9cdaab19c210887b92a2b01b6f32ec0c439b97dd
Indhuja-developer9363 <gopalindhuja@gmail.com>
2026-06-30 14:11:48 +0530

    Polymorphism Overriding

---
+++ Poverride.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Animal{
+    void sound(){
+        System.out.println("Animal sound");
+    }
+}
+class Dog extends Animal{
+    @Override
+    void sound(){
+        System.out.println("Bark");
+    }
+}
+public class Poverride{
+    public static void main(String[]args){
+        Dog d = new Dog();
+        d.sound();
+    }
+}
